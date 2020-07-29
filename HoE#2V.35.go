05656c3c79f5ea360ccbb54dc55d3d2072105091
Joh Leonhardt <johfleonhardt@gmail.com>
2020-07-29 21:58:53 +1000

    Hands-on exercise #2 - Video .35

---
+++ HoE#2V.35.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	//Q: The compiler assigned values to the
+	//variables. What are these values called?
+	//A: They are called zero vales.
+}
