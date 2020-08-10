2dd1b597a28769a4989f4d8c0aca693f87b75b99
Joh Leonhardt <eruca@192-168-1-8.tpgi.com.au>
2020-08-10 12:00:14 +1000

    Hands-on exercise #16 - Video .71

--- HoE#16V.71.go
+++ HoE#16V.71.go
@@ -6,45 +6,6 @@ import (
 
 func main() {
 	for i := 10; i <= 100; i++ {
-		fmt.Printf("When %v gets divided by 4, the remainder (aka modulus) is %v\n", i, i%4)
+		fmt.Printf("When %v is divided by 4, the remainder (aka modulus) is %v\n", i, i%4)
 	}
 }
-
-//Test1
-//package main
-
-//import (
-//"fmt"
-//)
-
-//func main() {
-//for i := 10; i <= 100; i++ {
-//if i%4 != 0 {
-//continue
-//}
-//fmt.Println(i)
-//}
-//}
-
-//Test2
-//package main
-
-//import (
-//"fmt"
-//)
-
-//func main() {
-//x := 10
-//for {
-//x++
-//if x > 100 {
-//break
-//}
-
-//if x%4 != 0 {
-//continue
-//}
-
-//fmt.Println(x)
-//}
-//}
